5581c98088854c08357f95165ab900fe68d6e570
Sanika Dongre <sado9543@colorado.edu>
2018-08-01 12:25:30 -0600

    changes

--- project/capture.c
+++ project/capture.c
@@ -21,7 +21,7 @@
 #include <sys/msg.h>
 #include <sys/ipc.h>
 #include <mqueue.h>
-#include <opencv/highgui.h>
+#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/videoio.hpp>
 #define mq_ppm "/ppm_writer_mq"
